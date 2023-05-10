8acc0fbb2cc23e89e3b15892e3f23c923f42ed76
SHIYADBAVA <muhammedshiyadcm80@gmail.com>
2023-05-11 01:37:10 +0530

    Added new files to the project

---
+++ main.py
@@ -0,0 +1,14 @@
+def to_upper(name):
+    return name.upper()
+
+def say_hello(name):
+    # Use a breakpoint in the code line below to debug your script
+    print(f"Hello, {name.title()}!") # Press %F8 to toggle the breakpoint
+
+
+#Press the green button in the gutter to run the script
+if __name__ == '__main__':
+    name = "Shiyad"
+    say_hello(name)
+    up = to_upper(name)
+    print(up)
